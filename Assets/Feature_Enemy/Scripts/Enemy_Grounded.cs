772fe66ee30c3836d4d86397c06057da157800b5
GongJaCh <81235791+ghostlink77@users.noreply.github.com>
2025-07-31 06:08:01 +0900

    Enemy_Grounded
    땅을 배회하는 기본적인 적(ex: 굼바)를 간단하게 구현
    앞이 낭떠러지면 방향을 바꿈
    애니메이션, 플레이어 탐지 구현 필요

---
+++ Assets/Feature_Enemy/Scripts/Enemy_Grounded.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Enemy_Grounded : Enemy
+{
+    private bool isWalking = false;
+
+    protected override void move()
+    {
+        int direction = isFacingRight ? 1 : -1;
+        rigid.linearVelocityX = moveSpeed * direction;
+
+        isWalking = true;
+    }
+    bool IsGroundAhead()
+    {
+        Vector2 frontPoint = new Vector2(rigid.position.x + (isFacingRight ? 0.5f : -0.5f), rigid.position.y);
+        RaycastHit2D hit = Physics2D.Raycast(frontPoint, Vector2.down, 1f, LayerMask.GetMask("Ground"));
+
+        return hit.collider != null;
+    }
+
+    void FixedUpdate()
+    {
+        move();
+        if (!IsGroundAhead())
+        {
+            Flip();
+        }
+    }
+}
