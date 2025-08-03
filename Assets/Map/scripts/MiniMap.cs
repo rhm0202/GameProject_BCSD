c3d082b5b489e05b4a0e67cd32f1bc37486f85b6
coral127 <sasahappy90@gmail.com>
2025-08-03 22:37:24 +0900

    minimap_scripting

--- Assets/Map/scripts/MiniMap.cs
+++ Assets/Map/scripts/MiniMap.cs
@@ -1,11 +1,32 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MiniMap : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public RenderTexture minimapRenderTexture;
+    public RawImage minimapDisplay;
+    public Transform playerTransform;
+
+    private Texture2D discoveryTexture;
+    private int textureWidth, textureHeight;
+
+    public int revealRadius = 10;
+    public float revealSpeed = 0.5f;
+
+
     void Start()
     {
-        
+        textureHeight = minimapRenderTexture.height;
+        textureWidth = minimapRenderTexture.width;
+
+        discoveryTexture = new Texture2D(textureWidth, textureHeight, TextureFormat.ARGB32, false);
+        Color[] colors = new Color[textureWidth * textureHeight];
+        for(int i = 0; i < colors.Length; i++)
+        {
+            colors[i] = Color.black;
+        }
+        discoveryTexture.SetPixels(colors);
+        discoveryTexture.Apply();
     }
 
     // Update is called once per frame
