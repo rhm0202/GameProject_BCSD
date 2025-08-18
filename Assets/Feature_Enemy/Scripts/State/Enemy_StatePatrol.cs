03e25e8441ce1ae2652900a5e58d99145f6025f0
GongJaCh <81235791+ghostlink77@users.noreply.github.com>
2025-08-18 19:39:17 +0900

    enemy 살짝 변경
    살짝 변경

--- Assets/Feature_Enemy/Scripts/State/Enemy_StatePatrol.cs
+++ Assets/Feature_Enemy/Scripts/State/Enemy_StatePatrol.cs
@@ -4,6 +4,9 @@ public class Enemy_StatePatrol : IState
 {
     private Enemy enemy;
 
+    float timer;
+    float waitingTime;
+
     public Enemy_StatePatrol(Enemy enemy)
     {
         this.enemy = enemy;
@@ -12,10 +15,17 @@ public class Enemy_StatePatrol : IState
     public void Enter()
     {
         enemy.ChangeAnimation("Walk");
+        timer = 0f;
+        waitingTime = Random.Range(1f, 4f); // Random patrol time between 1 and 3 seconds
     }
 
     public void Update()
     {
+        timer += Time.deltaTime;
+        if (timer >= waitingTime)
+        {
+            enemy.stateMachine.TransitionTo(enemy.stateMachine.stateIdle);
+        }
         enemy.Patrol();
         if (enemy.DetectPlayer() && enemy.isChasingPlayer)
         {
